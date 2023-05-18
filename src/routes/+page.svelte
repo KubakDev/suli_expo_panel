d54b56523ea1c5dee67f3e3dc26f0ac76d4dfbf5
Arman Hadi <arman.kubak@gmail.com>
2023-05-18 09:31:36 +0300

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,10 @@
 	import ToastComponent from '$lib/components/reusables/toastComponent.svelte';
 	import { Button } from 'flowbite-svelte';
 	import { addNewToast } from '../stores/toastStore';
-
 	let conf = {
 		toolbar: 'undo redo',
 		menubar: true
 	};
-
 	let value = 'some content';
 	let test = ['test', 'test2', 'test3', 'test4', 'test5'];
 	function showToast(message: string) {
