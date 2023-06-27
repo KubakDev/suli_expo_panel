f53cc3fe5b1c13982605eea9333b48e92d7402e0
shajwan2023 <shajwan.namiq2023@gmail.com>
2023-06-27 13:38:53 +0300

    update pagination

--- src/lib/components/pagination/Pagination.svelte
+++ src/lib/components/pagination/Pagination.svelte
@@ -1,69 +1,67 @@
 <script lang="ts">
-	import PaginationButton from './PaginationButton.svelte';
-
 	export let currentPage: number;
 	export let totalPages: number;
 	export let goToPage: any;
 </script>
 
-<div class="py-5 flex justify-end items-center">
-	<button
-		on:click={() => goToPage(currentPage - 1)}
-		disabled={currentPage === 1}
-		class="border bg-white hover:bg-gray-100 text-black py-2 px-4 rounded-l-md"
-	>
-		<div>
-			<svg
-				width="20px"
-				height="20px"
-				viewBox="0 0 1024 1024"
-				class="icon"
-				version="1.1"
-				xmlns="http://www.w3.org/2000/svg"
-				fill="#65686c"
-			>
-				<g id="SVGRepo_bgCarrier" stroke-width="0" />
-				<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
-				<g id="SVGRepo_iconCarrier">
-					<path
-						d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z"
-						fill="#65686c"
-					/>
-				</g>
-			</svg>
-		</div>
-	</button>
-
-	<div>
-		{#each Array.from({ length: totalPages }, (_, i) => i + 1) as page}
-			<PaginationButton {page} {currentPage} onClick={() => goToPage(page)} />
-		{/each}
+<div class="flex justify-end items-center">
+	<div class="flex items-center justify-center">
+		<span class="text-gray-700 mx-2">{currentPage} </span> of
+		<span class="text-gray-400 mx-2"> {totalPages}</span>
 	</div>
+	<div class="py-5 flex justify-center items-center">
+		<button
+			on:click={() => goToPage(currentPage - 1)}
+			disabled={currentPage === 1}
+			class="border bg-white hover:bg-gray-100 text-black py-2 px-4 rounded-l-md"
+		>
+			<div>
+				<svg
+					width="20px"
+					height="20px"
+					viewBox="0 0 1024 1024"
+					class="icon"
+					version="1.1"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="#65686c"
+				>
+					<g id="SVGRepo_bgCarrier" stroke-width="0" />
+					<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+					<g id="SVGRepo_iconCarrier">
+						<path
+							d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z"
+							fill="#65686c"
+						/>
+					</g>
+				</svg>
+			</div>
+		</button>
 
-	<button
-		on:click={() => goToPage(currentPage + 1)}
-		class="border bg-white hover:bg-gray-100 text-black py-2 px-4 rounded-r-md"
-		disabled={currentPage == totalPages}
-	>
-		<div>
-			<svg
-				width="20px"
-				height="20px"
-				viewBox="0 0 1024 1024"
-				class="icon"
-				version="1.1"
-				xmlns="http://www.w3.org/2000/svg"
-				fill="#000000"
-			>
-				<g id="SVGRepo_bgCarrier" stroke-width="0" />
-				<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
-				<g id="SVGRepo_iconCarrier">
-					<path
-						d="M256 120.768L306.432 64 768 512l-461.568 448L256 903.232 659.072 512z"
-						fill="#65686c"
-					/>
-				</g>
-			</svg>
-		</div>
-	</button>
+		<button
+			on:click={() => goToPage(currentPage + 1)}
+			class="border bg-white hover:bg-gray-100 text-black py-2 px-4 rounded-r-md"
+			disabled={currentPage == totalPages}
+		>
+			<div>
+				<svg
+					width="20px"
+					height="20px"
+					viewBox="0 0 1024 1024"
+					class="icon"
+					version="1.1"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="#000000"
+				>
+					<g id="SVGRepo_bgCarrier" stroke-width="0" />
+					<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+					<g id="SVGRepo_iconCarrier">
+						<path
+							d="M256 120.768L306.432 64 768 512l-461.568 448L256 903.232 659.072 512z"
+							fill="#65686c"
+						/>
+					</g>
+				</svg>
+			</div>
+		</button>
+	</div>
 </div>
