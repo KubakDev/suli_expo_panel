56b305061e15d195ed45759e22777e7b58477e93
RovarQRoom <rovar.07000120@outlook.com>
2023-07-11 15:47:43 +0300

    Fixed Routes

--- src/routes/dashboard/web_builder/+layout.ts
+++ src/routes/dashboard/web_builder/+layout.ts
@@ -1,35 +1,3 @@
-export const load = async () => {
-	let sideBarPage = [
-		{
-			title: 'news',
-			url: '/dashboard/web_builder/news',
-			icon: 'svelte-icons-pack/bs/BsTrash'
-		},
-		{
-			title: 'service',
-			url: '/dashboard/web_builder/service',
-			icon: 'svelte-icons-pack/bs/BsTrash'
-		},
-
-<<<<<<< HEAD
-		{
-			title: 'exhibition',
-			url: '/dashboard/web_builder/exhibition',
-			icon: 'svelte-icons-pack/bs/BsTrash'
-		},
-		{
-			title: 'web builder',
-			url: '/dashboard/web_builder',
-			icon: 'svelte-icons-pack/bs/BsTrash'
-		},
-		{
-			title: 'theme color',
-			url: '/dashboard/web_builder/theme_color'
-		}
-	];
-	return { sideBarPage };
-};
-=======
 export const load = (async () => {
     let sideBarPage = [
         {
@@ -63,4 +31,3 @@ export const load = (async () => {
     ]
     return { sideBarPage };
 });
->>>>>>> 6575a20c4f5adb435220fb26782bd43d38879d68
