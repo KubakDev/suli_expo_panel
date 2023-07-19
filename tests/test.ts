37ac9b281c0cebb1e0908d02ba1de930929ebeea
Rovar Kamil <105520245+RovarQRoom@users.noreply.github.com>
2023-07-19 10:01:46 +0300

    Update test.ts

--- tests/test.ts
+++ tests/test.ts
@@ -3,5 +3,5 @@ import { expect, test } from '@playwright/test';
 test('about page has expected h1', async ({ page }) => {
 	await page.goto('/about');
 	await expect(page.getByRole('heading', { name: 'About this app' })).toBeVisible();
-	// useless game we are all playing.
+	// useless game 
 });
