40e350312490fe2c055fe07bb6768b3d1fff7dcd
shajwan2023 <shajwan.namiq2023@gmail.com>
2023-08-14 08:59:18 +0300

    Add reservation detail

--- src/models/reservationModel.ts
+++ src/models/reservationModel.ts
@@ -1,16 +1,15 @@
-
 import type { CompanyType } from './companyModel';
 import type { ExhibitionModel } from './exhibitionTypeModel';
 import type { ReservationStatusEnum } from './reservationEnum';
 
 export interface Reservation {
-	id: number;
-	exhibition_id: number;
-	object_id: number;
-	comment: string;
-	services: string[];
-	company_id: number;
-	company: CompanyType;
-	exhibition: ExhibitionModel;
-	status: ReservationStatusEnum;
+	id?: number;
+	exhibition_id?: number;
+	object_id?: number;
+	comment?: string;
+	services?: string[];
+	company_id?: number;
+	company?: CompanyType;
+	exhibition?: ExhibitionModel;
+	status?: ReservationStatusEnum;
 }
