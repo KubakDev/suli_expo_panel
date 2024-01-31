dee529a4b6177285b5eec01abb476ea074b27cb3
RovarQRoom <rovar.07000120@outlook.com>
2024-01-31 15:35:34 +0300

    Update EditorComponent.svelte toolbar configuration

--- src/lib/components/EditorComponent.svelte
+++ src/lib/components/EditorComponent.svelte
@@ -6,9 +6,10 @@
 
 	const conf = {
 		plugins:
-			'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+			'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+		menubar: 'file edit view insert format tools table help',
 		toolbar:
-			'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+			'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
 		height: 500
 	};
 </script>
