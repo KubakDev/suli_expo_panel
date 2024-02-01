f2bdbbe5a5adc668191de9f0c0638bc9c53b91df
Rovar Kamil <105520245+RovarQRoom@users.noreply.github.com>
2024-02-01 10:42:01 +0300

    Update EditorComponent.svelte

--- src/lib/components/EditorComponent.svelte
+++ src/lib/components/EditorComponent.svelte
@@ -4,6 +4,7 @@
 	export let langData: any;
 	export let isFormSubmitted: boolean;
 
+
 	const conf = {
 		plugins:
 			'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
